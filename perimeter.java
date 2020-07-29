18ae5e626b2f2fe0ccf9711ec8951a89b782e08a
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-07-28 21:10:06 -0700

    Add files via upload

---
+++ perimeter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worksheet1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author home
+ */
+public class perimeter {
+    public static void main(String[] args){
+        
+    Scanner obj=new Scanner(System.in);
+    System.out.println("enter a value");
+    int a=obj.nextInt();
+    System.out.println("perimeter"+(4*a));
+    }
+}
