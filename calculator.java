d33bcfb8a454adb71435f8f00b0d19a0407159ac
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-07-28 20:59:01 -0700

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worksheet1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author home
+ */
+public class calculator {
+  public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter a and b");
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        char c=obj.next().charAt(0);
+        
+        switch(c)
+        {
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            default:
+                 System.out.println("function cannot be performed");
+                break;
+        }
+    }
+    
+}   
