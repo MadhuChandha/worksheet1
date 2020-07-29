42228bc7a9f60a7ccc0fec661579f33bd92e73ae
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-07-28 21:15:03 -0700

    Add files via upload

---
+++ powerofnumber.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worksheet1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author home
+ */
+public class powerofnumber {
+    public static void main(String[] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the values of a and n");
+        int a=obj.nextInt();
+        int n=obj.nextInt();
+        int i,sum=1;
+            
+        for(i=0;i<n;i++)
+        {
+            sum=sum*a;
+        }
+       System.out.println(sum);
+        }
+        
+            }
