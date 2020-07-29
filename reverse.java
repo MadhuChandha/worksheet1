00d05bd63ebfe35fbc1fb004e6a280ce9d8eda4f
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-07-28 21:21:15 -0700

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worksheet1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author home
+ */
+public class reverse {
+    public static void main(String[] args)
+    {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("enter n");
+    int n=obj.nextInt();
+    System.out.println("enter the array elements");
+    int a[]=new int[10];
+    int i;
+    {
+    
+    for(i=0;i<n;i++)
+    {
+        a[i]=obj.nextInt();
+       
+    }
+    for(i=n-1;i>=0;i--)//for reverse order
+    {
+        System.out.println(a[i]);
+    }
+    }
+    }
+}
